b26f6b6b5a5efa6c6642051d4c7a55183437b9a8
Gabriel Nestor Brufau <70489784+qukiiignb@users.noreply.github.com>
2023-06-22 19:35:20 -0300

    Update Main.scala

--- UPLOCK-SOLUTIONS/1/A/hello/src/main/scala/Main.scala
+++ UPLOCK-SOLUTIONS/1/A/hello/src/main/scala/Main.scala
@@ -1,16 +1,11 @@
 
-/*@ main
- * @ main annotation is used to define the entry point of a Scala application*/
-/* def
- * def: It is the keyword used to define a method.*/
-/* Unit
- * When a function or method doesn't return any useful result, its return type is usually specified as Unit.*/
-/* println
- * Prints out an object to the default output*/
+// @ main - annotation is used to define the entry point of a Scala application
+// def -  It is the keyword used to define a method
+// Unit - It's similar to voil absence of meaningful .-When a function or method doesn't returnany useful result, its return type is usually specified as Unit.
+// println - out an object to the default output
 @main def hello: Unit =
   println("Hello world!")
   println(msg)
 
-/* msg
- * msg" is a commonly used shorthand or abbreviation for "message." It is often used as a variable name or parameter name*/
+// msg -  is a commonly used shorthand or abbreviation for "message." It is often used as a variable name or parameter name
 def msg = "I was compiled by Scala 3. :)"
